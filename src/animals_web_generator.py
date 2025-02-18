f8545c2c3c0577df6c81b6f885731d779051e4a9
Raffael E <raffa_e@web.de>
2025-02-18 12:18:29 +0100

    Load html template, generate html with animal names, write new html file containing animal data

--- src/animals_web_generator.py
+++ src/animals_web_generator.py
@@ -1,17 +1,43 @@
 import json
 
+DATA_FILE = "animals_data.json"
+TEMPLATE_FILE = "animals_template.html"
+OUTPUT_FILE = "animals.html"
+PLACEHOLDER = "__REPLACE_ANIMALS_INFO__"
 
-def load_data(file_path):
-    with open(file_path, "r") as file:
+
+def load_animal_data():
+    with open(DATA_FILE, "r") as file:
         return json.load(file)
 
 
-def main():
-    DATA_FILE = "animals_data.json"
-    data = load_data(DATA_FILE)
+def load_template_html():
+    with open(TEMPLATE_FILE, "r") as file:
+        return file.read()
 
-    for fox in data:
-        print(fox["name"])
+
+def write_output_html(content, ):
+    with open(OUTPUT_FILE, "w") as file:
+        file.write(content)
+
+
+def generate_html(template, animal_data):
+    fox_names = []
+
+    for fox in animal_data:
+        fox_names.append("<li>")
+        fox_names.append(fox["name"])
+        fox_names.append("</li>")
+
+    output = "\n".join(fox_names)
+    return template.replace(PLACEHOLDER, output)
+
+
+def main():
+    data = load_animal_data()
+    template = load_template_html()
+    content = generate_html(template, data)
+    write_output_html(content)
 
 
 if __name__ == '__main__':
