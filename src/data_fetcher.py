abc574402957ae7d9e8e74b83af164927e6d7b85
Raffael E <raffa_e@web.de>
2025-02-21 18:46:20 +0100

    Raise error if no or incorrect API key was provided

--- src/data_fetcher.py
+++ src/data_fetcher.py
@@ -10,6 +10,10 @@ def get_animal_data(animal):
     url = f"{API_NINJAS}/{ANIMALS}?name={animal}"
     response = requests.get(url, headers={"X-Api-Key": API_KEY})
 
+    if response.status_code == 400:
+        raise PermissionError(
+            "Invalid or no API key provided. Make sure .env has valid API_KEY property.")
+
     if not response.status_code == 200:
         return []
 
